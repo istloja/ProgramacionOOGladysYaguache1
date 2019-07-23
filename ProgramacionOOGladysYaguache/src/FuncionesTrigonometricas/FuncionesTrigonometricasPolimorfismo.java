528e417373e24019bbf0cc380d571c875ae92a9b
HOGAR <HOGAR@DESKTOP-TFPS095>
2019-07-23 13:03:20 -0500

    Sincronizando el mensaje

--- ProgramacionOOGladysYaguache/src/FuncionesTrigonometricas/FuncionesTrigonometricasPolimorfismo.java
+++ ProgramacionOOGladysYaguache/src/FuncionesTrigonometricas/FuncionesTrigonometricasPolimorfismo.java
@@ -5,22 +5,22 @@ package FuncionesTrigonometricas;
 public class FuncionesTrigonometricasPolimorfismo {
      public static void main(String[] args) {
         
-        double resultado =0;
+        double result =0;
         double anguloenGrados = 90;
         double angulosenRadianes = Math.toRadians(anguloenGrados);
         
         
         //Cos
-        resultado = Math.cos(angulosenRadianes);
-        System.out.println("Cos de " + anguloenGrados + "° = " +resultado );
+        result = Math.cos(angulosenRadianes);
+        System.out.println("Cos de " + anguloenGrados + "° = " +result );
         
         //Sen
-        resultado = Math.sin(angulosenRadianes);
-        System.out.println("Sen de " + anguloenGrados + "° = " +resultado );
+        result = Math.sin(angulosenRadianes);
+        System.out.println("Sen de " + anguloenGrados + "° = " +result );
         
          //Tang
-        resultado = Math.tan(angulosenRadianes);
-        System.out.println("Tang de " + anguloenGrados + "° = " +resultado );
+        result= Math.tan(angulosenRadianes);
+        System.out.println("Tang de " + anguloenGrados + "° = " +result );
         
     }
     
--- ProgramacionOOGladysYaguache/src/FuncionesTrigonometricas/constantes.java
+++ ProgramacionOOGladysYaguache/src/FuncionesTrigonometricas/constantes.java
@@ -5,8 +5,8 @@ package FuncionesTrigonometricas;
 public class constantes {
    
     public static void main(String[] args) {
-        System.out.println("El numero e:"+Math.E);
-        System.out.println("El numero pi:"+Math.PI);
+        System.out.println("El num e:"+Math.E);
+        System.out.println("El num pi:"+Math.PI);
  
 }
     }
