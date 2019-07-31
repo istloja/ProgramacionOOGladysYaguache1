cfdc3902c3d5ca5e9a3bb6123415f3bbc56677f2
HOGAR <HOGAR@DESKTOP-TFPS095>
2019-07-31 02:16:03 -0500

    Sincronizando el mensaje

---
+++ ProgramacionOOGladysYaguache/src/DEBER1/Cuenta.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DEBER1;
+
+
+public class Cuenta {
+     private String nombreDelCliente;
+    private String numeroDeCuenta;
+    private double tipoDeInteres;
+    private double saldo;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(String nombreDelCliente, String numeroDeCuenta, double tipoDeInteres, double saldo) {
+        this.nombreDelCliente = nombreDelCliente;
+        this.numeroDeCuenta = numeroDeCuenta;
+        this.tipoDeInteres = tipoDeInteres;
+        this.saldo = saldo;
+    }
+
+    public String getNombreDelCliente() {
+        return nombreDelCliente;
+    }
+
+    public void setNombreDelCliente(String nombreDelCliente) {
+        this.nombreDelCliente = nombreDelCliente;
+    }
+
+    public String getNumeroDeCuenta() {
+        return numeroDeCuenta;
+    }
+
+    public void setNumeroDeCuenta(String numeroDeCuenta) {
+        this.numeroDeCuenta = numeroDeCuenta;
+    }
+
+    public double getTipoDeInteres() {
+        return tipoDeInteres;
+    }
+
+    public void setTipoDeInteres(double tipoDeInteres) {
+        this.tipoDeInteres = tipoDeInteres;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean Ingreso(double cantidad) {
+
+        if (cantidad > 0) {
+            double Ingreso = getSaldo() + cantidad;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean Reitegro(double cantidad) {
+        if (saldo >= cantidad) {
+            double reintegro = getSaldo() - cantidad;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+    
+
